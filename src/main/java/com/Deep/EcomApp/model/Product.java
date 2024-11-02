477319a7a84b81f9b53b96af5817102ccab3b576
Deep saha <156496540+Deep-Saha1925@users.noreply.github.com>
2024-11-02 10:53:52 +0530

    Add files via upload

---
+++ src/main/java/com/Deep/EcomApp/model/Product.java
@@ -0,0 +1,32 @@
+package com.Deep.EcomApp.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@Entity
+public class Product {
+
+    Product(){
+        System.out.println("Inside product");
+    }
+
+    @Id
+    private int id;
+    private String name;
+    private String description;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+    private Date releaseDate;
+    private boolean available;
+    private int quantity;
+
+
+}
